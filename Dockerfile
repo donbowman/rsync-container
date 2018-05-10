731e8a96a95ad858fea0fcbe543c579a42d88a93
Don Bowman <don@agilicus.com>
2018-05-09 21:21:02 -0400

    Fix LANG/locale to be C Need a shell (not nologin)

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update \
     && apt-get --no-install-recommends -y install openssh-server rsync \
     && mkdir -p /sync /home/rsync/.ssh \
-    && useradd -d /home/rsync -g 0 -M -N -o -s /usr/sbin/nologin -u 0 rsync \
+    && useradd -d /home/rsync -g 0 -M -N -o -s /bin/sh -u 0 rsync \
     && chmod 700 /home/rsync/.ssh /home/rsync \
     && rm -rf /var/lib/apt/lists/*
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Setup auth keys"
-echo "command=\"/scripts/rrsync -ro /sync/\",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding $SSH_PUBKEY" > /home/rsync/.ssh/authorized_keys
+echo "command=\"LANG=C /scripts/rrsync -ro /sync/\",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding $SSH_PUBKEY" > /home/rsync/.ssh/authorized_keys
 echo > /home/rsync/.ssh/authorized_keys2
 
 SSHD_PORT=${SSHD_PORT:-2222}
